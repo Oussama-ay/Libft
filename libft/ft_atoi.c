151c63cac24f37b94387226317f5beedfd628ae1
Oussama-ay <ayyouboussama9@gmail.com>
2024-09-26 17:24:39 -0400

    done

---
+++ libft/ft_atoi.c
@@ -0,0 +1,21 @@
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	result;
+	int	sig;
+
+	i = 0;
+	while ((nptr[i] >= '\r' && nptr[i] <= '\t') || nptr[i] == ' ')
+		i++;
+	sig = 1;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sig *= -1;
+		i++;
+	}
+	result = 0;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+		result[i] = result[i] * 10 + resul[i++] - '0';
+	return (sig * result);
+}
