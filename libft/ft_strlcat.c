55c365fa277f37b183f264caccd97ec8baa9f3b3
Oussama-ay <ayyouboussama9@gmail.com>
2024-09-27 05:36:26 -0400

    fix

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -2,17 +2,17 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
 	int	j;
 
-	i = 0;
-	while (dest[i])
-		i++;
-	if (size == 0 || size < i)
-		return
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size == 0 || size <= dst_len)
+		return (src_len + size);
 	j = 0;
-	while (j < size - 1 && src[j])
-		dest[i++] = src[j++];
-	dest[i] = 0;
-	return (i);
+	while (j < size - dst_len - 1 && src[j])
+		dest[j + dst_len] = src[j++];
+	dest[j + dst_len] = 0;
+	return (dst_len + src_len);
 }
---
+++ libft/ft_substr.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int	size;
+	int	i;
+	char	*str;
+
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	str = malloc(len + 1);
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i + start] && i < len)
+		str[i] = s[start + i++];
+	str[i] = 0;
+	return (str);
+}
