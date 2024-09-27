6e8f93f632ac64bbad018e7f0d8d2897aadfbf48
Oussama-ay <ayyouboussama9@gmail.com>
2024-09-27 05:57:03 -0400

    done

---
+++ libft/ft_split.c
@@ -0,0 +1,66 @@
+int	is_delimiter(char s, char c)
+{
+	return (s == c);
+}
+
+int	ft_count(chat *str, char c)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		while (*str && is_delimiter(*str, c))
+			str++;
+		if (*str)
+			count++;
+		while (*str && !is_delimiter(*str, c))
+			str++;
+	}
+	return (count);
+}
+
+char	*ft_word(char *str, char c)
+{
+	int	len;
+	int	i;
+	char	*word;
+
+	len = 0;
+	while (str[len] && !is_delimiter(str[len], c))
+		len++;
+	word = malloc(len + 1);
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+		word[i] = str[i++];
+	word[i] = 0;
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**result;
+	int	nbr_words;
+	int	i;
+
+	nbr_words = ft_count(s, c);
+	result = malloc(sizeof(char *) * (nbr_words + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s && is_delimiter(*s, c))
+			s++;
+		if (*s)
+		{
+			result[i++] = ft_word(s, c);
+			while (*s && !is_delimiter(*s, c));
+				s++;
+		}
+	}
+	result[i] = 0;
+	return (result);
+}
