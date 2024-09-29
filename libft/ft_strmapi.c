2b592ce9d8d861c8ec6e4ce510dffbc01456ca79
Oussama-ay <ayyouboussama9@gmail.com>
2024-09-29 13:47:55 -0400

    .

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,13 +6,18 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	int	i;
 	char	*result;
 
+	if (!s)
+		return (ft_strdup(""));
 	size = ft_strlen(s);
 	result = malloc(size + 1);
 	if (!result)
 		return (NULL);
 	i = 0;
 	while (i < size)
-		result[i] = (*f)(i, result);
+	{
+		result[i] = (*f)(i, s[i]);
+		i++;
+	}
 	result[i] = 0;
 	return (result);
 }
